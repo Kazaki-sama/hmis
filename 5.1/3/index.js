8138866dd6de986565790ba1af394fcac89a9be2
Kazaki-sama <maharjansubham7@gmail.com>
2024-09-16 05:55:13 -0400

    HMIS pages

---
+++ 5.1/3/index.js
@@ -0,0 +1,10 @@
+const rows=document.querySelectorAll('tbody tr');
+const lastRow=rows[rows.length - 1]
+rows.forEach((row, index)=>{
+    if(index%2!=0){
+        row.classList.add('highlight')
+    }
+})
+
+
+lastRow.classList.remove('highlight')
